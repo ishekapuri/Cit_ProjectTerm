42a9211b326c33cf2410ad303f6717063f4883bf
Henry <skippy24680@gmail.com>
2025-05-07 10:12:58 -0700

    updated home page

--- routes/html.py
+++ routes/html.py
@@ -6,7 +6,21 @@ html_routes = Blueprint("html_routes", __name__)
 
 @html_routes.route("/")
 def home():
-    return render_template("home.html")
+    collections = db.session.execute(db.select(Collection)).scalars()
+    quizzes = db.session.execute(db.select(Quiz)).scalars()
+    return render_template("home.html", collections=collections, quizzes=quizzes)
+
+@html_routes.route("/makeQuiz", methods=["GET"])
+def make_quiz():
+    return render_template("makeQuiz.html")
+
+@html_routes.route("/makeQuiz", methods=["POST"])
+def make_quiz_post():
+    quiz_name = request.form.get("quiz_name")
+    quiz = Quiz(name=quiz_name)
+    db.session.add(quiz)
+    db.session.commit()
+    return render_template("makeQuiz.html", quiz=quiz)
 
 # COLLECTION LIST ==========================================================
 @html_routes.route("/collections")
@@ -21,12 +35,20 @@ def stacks(collection_name):
     stacks = db.session.execute(db.select(Stack).where(Stack.collection_id == collection_id)).scalars()
     return render_template("stacks.html", collection=collection_name, data=stacks)
 
-
 # # STACK DETAILS ==========================================================
 @html_routes.route("/collections/<string:collection_name>/<string:stack_name>")
 def cards(collection_name, stack_name):
     stack_id = (db.session.execute(db.select(Stack).where(Stack.name == stack_name)).scalar()).id
     cards = db.session.execute(db.select(Card).where(Card.stack_id == stack_id)).scalars()
     return render_template("cards.html", stack=stack_name, data=cards)
+
+@html_routes.route("/quizzes", methods=["GET"])
+def quizzes():
+    quizzes = db.session.execute(db.select(Quiz)).scalars()
+    return render_template("quizzes.html", data=quizzes)
+
+# @html_routes.route("/makeQuiz", methods=["POST"])
+# def make_quiz():
+
 
 
